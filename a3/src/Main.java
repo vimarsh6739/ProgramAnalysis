725140da98f85ec1d1606d0338418c824e9b26fc
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2021-04-24 11:47:56 +0530

    Added starter files for MHP

---
+++ a3/src/Main.java
@@ -0,0 +1,18 @@
+import syntaxtree.*;
+import visitor.DepthFirstVisitor;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try {
+            
+            new QParJavaParser(System.in);
+            Node root = QParJavaParser.Goal();
+            root.accept(new DepthFirstVisitor());
+            System.out.println("Program parsed successfully");
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
